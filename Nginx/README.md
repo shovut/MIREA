b5a6f92dae657e8408771c7e4527012af4faedd6
shovut <tbikba4ok@mail.ru>
2019-12-21 01:22:43 +0300

    Nginx

---
+++ Nginx/README.md
@@ -0,0 +1,2 @@
+# Run
+docker-compose build && docker-compose up
---
+++ Nginx/backend/all/data.py
@@ -0,0 +1,32 @@
+from django.http import JsonResponse
+import json
+
+
+class Data:
+    js = [
+        {
+            'id': '1',
+            'type': 'cat',
+            'color': 'black',
+        },
+        {
+            'id': '2',
+            'type': 'cat',
+            'color': 'white',
+        },
+        {
+            'id': '3',
+            'type': 'dog',
+            'color': 'rainbow',
+        },
+        {
+            'id': '4',
+            'type': 'rabbit',
+            'color': 'white',
+        },
+        {
+            'id': '5',
+            'type': 'crocodile',
+            'color': 'green',
+        },
+    ]
